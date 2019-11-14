9f805c9acfd11a5980e15bf60372c2f85ac58fdf
Dan Marshall <danmar@microsoft.com>
2019-11-13 22:06:59 -0800

    add conversions

--- packages/vega-lite-unit/src/convert.ts
+++ packages/vega-lite-unit/src/convert.ts
@@ -1,41 +1,84 @@
+import * as Vega from 'vega-typings';
 import * as VegaLite from 'vega-lite';
-import * as Vega from 'vega-typings'
 
 export type UnitStyle = 'square' | 'treemap' | 'normalize';
+
+interface TransformItem<T extends Vega.Transforms> {
+    transform: T;
+    i: number;
+}
 
 export function unitize(inputSpec: VegaLite.TopLevelSpec, quantitativeX: boolean, unitStyle: UnitStyle) {
     const output = VegaLite.compile(inputSpec);
 
     const vegaSpec = output.spec as Vega.Spec;
-
-    //TODO unitize
-
     const data0 = vegaSpec.data[0];
 
-    addIdentifierTransform(data0);
+    //add identifier preceding aggregate
+    const idts: Vega.IdentifierTransform = {
+        type: 'identifier',
+        as: 'id'
+    };
+    data0.transform.unshift(idts);
 
-
+    const aggregateItem = findTransformByType<Vega.AggregateTransform>(data0, 'aggregate') || { transform: null, i: 0 };
 
     //change aggregate to window
-    //change aggregation fields to "column", group by facet, then by category / bin
-    //remove stack
-    //change mark to xy, hw
-    //change scale to __count only
+    const windowTransform: Vega.WindowTransform = {
+        type: 'window',
 
+        //TODO should some be removed?
+        //TODO change aggregation fields to "column", 
+        //group by facet, then by category / bin
+        groupby: aggregateItem.transform.groupby,
+
+        ops: ["count"],
+
+        //TODO sort needs to account for stacked bars
+        sort: { "field": ["Class", "id"], "order": ["descending", "ascending"] },
+
+        fields: ["id"],
+        as: ["__count"]
+    };
+    data0.transform[aggregateItem.i] = windowTransform;
+
+    data0.transform.push({
+        type: "extent",
+        field: "__count",
+        signal: "maxcount"
+    })
+
+    //remove stack
+    const stackItem = findTransformByType<Vega.StackTransform>(data0, 'stack');
+    if (stackItem) {
+        data0.transform.splice(stackItem.i, 1);
+    }
+
+    //TODO change mark to xy, hw
+
+    //change y scale to __count only
+    const yScale = findScaleByName(vegaSpec.scales, 'y') as Vega.LinearScale;
+    const yDomain = yScale.domain as Vega.DataRef;
+    yDomain.field = "__count";
+    const yDomain2 = yScale.domain as Vega.MultiDataRef;
+    delete yDomain2.fields;
 
     return vegaSpec;
 }
 
-function addIdentifierTransform(d: Vega.Data) {
-    const aggPos = getAggregateTransformPosition(d) || 0;
-    d.transform.splice(aggPos, 0, { type: 'identifier', as: 'id' });
+function findTransformByType<T extends Vega.Transforms>(d: Vega.Data, type: 'aggregate' | 'stack') {
+    for (let i = 0; i < d.transform.length; i++) {
+        let transform = d.transform[i];
+        if (transform.type === type) {
+            return { transform, i } as TransformItem<T>;
+        }
+    }
 }
 
-function getAggregateTransformPosition(d: Vega.Data) {
-    for (let i = 0; i < d.transform.length; i++) {
-        let t = d.transform[i];
-        if (t.type === 'aggregate') {
-            return i;
+function findScaleByName(scales: Vega.Scale[], name: string) {
+    for (let i = 0; i < scales.length; i++) {
+        if (scales[i].name === name) {
+            return scales[i];
         }
     }
 }
